0ce6b424d309f2cf383b1117f2156708ddaab353
Joaquin <a22joaquinrs@iessanclemente.net>
2023-03-02 16:40:38 +0000

    presta sin https

---
+++ prestashop/var/cache/prod/appParameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'db',
+    'database_port' => '',
+    'database_name' => 'dbname',
+    'database_user' => 'user',
+    'database_password' => 'abc123.',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '9O9pp4dkXqV4AYg8rrhocRkK5DI9tkaPNsIwWxpRzqZNRfsKzGtr6DBz9y4MPVLF',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-03-02',
+    'locale' => 'es-ES',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'yB8kM1QzMTVJ1tjc37dVaNr4CGXnEh7TYEc7I1Zk0LoDFtM9NfSxMrheP1Mt2pjQ',
+    'cookie_iv' => '9UQbr9Qq43E656ddU84OohsW4XzIKSVQ',
+    'new_cookie_key' => 'def00000ec44989a5b6cc4eb920300ecd91e00befa842c73b901558d7064e3e8f48e6425395494fa92f139babe30dc515330aea669d82ad0d6b32afd30a18812a9e81eab',
+  ),
+);
